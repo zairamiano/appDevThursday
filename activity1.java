fba99274dcb691e794602d61557e3e9ac91f919f
Jay-Mark25 <jrpiano@paterostechnologicalcollege.edu.ph>
2025-02-20 20:19:15 +0800

    Added name: Jay Mark R. Piano

--- activity1.java
+++ activity1.java
@@ -1,7 +1,6 @@
 public class Activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
-        // Collaborators: Add your names below using the format:
-        // System.out.println("Your Full Name - Section");
+        System.out.println("Jay Mark R. Piano - 2L");
      }
 }
