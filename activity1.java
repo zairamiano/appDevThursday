71c79ce8560dae9101ce0aa939769cd6197c48ca
isel111 <tapgranado@paterostechnologicalcollege.edu.ph>
2025-02-20 21:35:40 +0800

    activity1.java

--- activity1.java
+++ activity1.java
@@ -27,7 +27,8 @@ public class Activity1 {
 
       System.out.println("Justin Emmanuel P. Delbo - BSIT2L");
 	  System.out.println("Kleine Paul Clark - BSIT-2L"); 
-
+	    
+ 	System.out.println("Treselle Anne P. Granado - 2k");
 
 
 
