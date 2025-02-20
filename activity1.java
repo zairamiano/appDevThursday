ed535aa199487561c0ace67526061113274fc055
kurt <kbclark@paterostechnologocalcollege.edu.ph>
2025-02-20 19:59:49 +0800

    Added name:Kurt Smith B. Clark

--- activity1.java
+++ activity1.java
@@ -3,5 +3,6 @@ public class Activity1 {
         System.out.println("List of Students:");
         // Collaborators: Add your names below using the format:
         // System.out.println("Your Full Name - Section");
+	System.out.println("Kurt Smith B. Clark - BSIT 2L");
      }
 }
