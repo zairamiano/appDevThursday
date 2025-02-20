97cb348eb54c933a06bbabc9f4d4ba1aa3f63ce5
Cerzo-Mark <mvcerzo@paterostechnologicalcollege.edu.ph>
2025-02-20 19:57:12 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -2,6 +2,7 @@ public class Activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
         // Collaborators: Add your names below using the format:
+        System.out.println("Mark Vincent Cerzo - BSIT-2K"); 
         // System.out.println("Your Full Name - Section");
      }
 }
