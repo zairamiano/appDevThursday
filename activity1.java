b79a0fa0200268061f94a047bb40cf05017d2ab8
Aliyahelysa <aessantander@paterostechnologicalcollege.edu.ph>
2025-02-21 11:15:22 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -28,7 +28,7 @@ public class Activity1 {
       System.out.println("Justin Emmanuel P. Delbo - BSIT2L");
 	  System.out.println("Kleine Paul Clark - BSIT-2L"); 
 
-	    System.out.rintln("Aliyah Elysa S. Santander"):
+	    System.out.rintln("Aliyah Elysa S. Santander - BSIT-2L"):
 
 
 
