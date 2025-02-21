e6d61f9a0cc4708e96e18b07e72e58abcdb41972
Iverson <itdeguzman@paterostechnologicalcollege.edu.ph>
2025-02-21 16:11:32 +0800

    Added name:Iverson De Guzman

--- activity1.java
+++ activity1.java
@@ -3,6 +3,6 @@ public class Activity1 {
         System.out.println("List of Students:");
         // Collaborators: Add your names below using the format:
         // System.out.println("Your Full Name - Section");
-	  System.out.println("Kleine Paul Clark - BSIT-2L"); 
+	  System.out.println("Iverson T. De Guzman - BSIT-2L"); 
      }
 }
