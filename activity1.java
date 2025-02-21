282974d32d4fb3638a657b68eeb78d4c10b49a82
jmhall12 <jmhallig@paterostechnologicalcollege.edu.ph>
2025-02-21 11:12:13 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -28,7 +28,7 @@ public class Activity1 {
       System.out.println("Justin Emmanuel P. Delbo - BSIT2L");
 	  System.out.println("Kleine Paul Clark - BSIT-2L"); 
 
-	    System.out.println("Aliyah Elysa S. Santander"):
+	    System.out.println("jommel M. Hallig - BSIT-2L"):
 
 
 
