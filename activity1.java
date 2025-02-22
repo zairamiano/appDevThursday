8e602b7068eeb594b2c1ffc101cf9ddb68051b2c
alinajerMocadiman32 <amocadiman@paterostechnologicalcollege.edu.ph>
2025-02-22 15:07:26 +0800

    Mocadiman

--- activity1.java
+++ activity1.java
@@ -65,7 +65,7 @@ System.out.println("Justin Emmanuel P. Delbo - BSIT2L");
 
 	System.out.println("Angel Mae Valdez - BSIT-2K");
 	
-        System.out.println("Alinajer T. Mocadiman - BSIT-2K");
+        System.out.println("Alinajer T. Mocadiman - BSIT-2L");
 
 
      }
