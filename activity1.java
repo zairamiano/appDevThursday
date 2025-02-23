7f0edf05693d4a2e2c0f0ce1b02a60b3664c0efc
ShaneArianne13 <spol@paterostechnologicalcollege.edu.ph>
2025-02-23 19:27:20 +0800

    Added name:Shane Arianne Pol

--- activity1.java
+++ activity1.java
@@ -4,7 +4,7 @@ public class Activity1 {
 
         // Collaborators: Add your names below using the format:
         // System.out.println("Your Full Name - Section");
-
+	   System.out.println("Shane Arianne Pol- BSIT-2L");
 	  System.out.println("Iverson T. De Guzman - BSIT-2L"); 
 
 
