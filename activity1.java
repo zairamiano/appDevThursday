78cd99ad58e0e35dec33dcb0464b786e85ab7926
Masagca-Jazmine <jamasagca@paterostechnologicalcollege.edu.ph>
2025-02-25 18:22:28 +0800

    Update activity1.java
    Jazmine Masagca

--- activity1.java
+++ activity1.java
@@ -64,6 +64,11 @@ System.out.println("Justin Emmanuel P. Delbo - BSIT2L");
 
 
 	System.out.println("Angel Mae Valdez - BSIT-2K");
+
+	System.out.println("Jazmine Masagca - BSIT-2K");
+
+	
+	    
 	
 
 
