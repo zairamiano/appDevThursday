223b2dce4493fa7ad2b298a6112012a28c51218c
kurt <kbclark@paterostechnologocalcollege.edu.ph>
2025-02-27 19:09:48 +0800

    Added ClarkKurt.java

---
+++ ClarkKurt.java
@@ -0,0 +1,5 @@
+public class ClarkKurt {
+    public static void display() {
+        System.out.println("Hello! This is ClarkKurt contributing to the project.");
+    }
+}
---
+++ week4.java
@@ -0,0 +1,11 @@
+public class week4 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+       
+        // Call each student's method
+        student1.display();
+       
+        // More students will add their method calls here...
+	ClarkKurt.display();
+    }
+}
