d18b1d3ecf5b23ca16c473ca41c85c70ff961308
Bermejo28 <lbbermejopaterostechnologicalcollege.edu.ph>
2025-02-27 20:27:19 +0800

    Added Bermejo.java

--- week4.java
+++ week4.java
@@ -6,16 +6,9 @@ public class week4 {
 
         //student1.display();
        // Add your method below
-         
         // More students will add their method calls here...
 
-         Piano.display();
-         Geringer.display();
-         ClarkKurt.display();
-         Hernandez.display(); 
-         Piano.display();
-Hallig.display()
-
+        Hallig.display()
 	Smith.display();
 	Piano.display();
 	Geringer.display();
@@ -23,6 +16,7 @@ Hallig.display()
 	Hernandez.display(); 
 	Piano.display();
 	KPClark.display();
+	Bermejo.display();
 
     }
 }
