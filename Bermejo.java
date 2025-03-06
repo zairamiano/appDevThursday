557943496ad1c1fc57178b583899b86a9260c96c
Bermejo <lbbermejo@paterostechnologicalcollege.edu.ph>
2025-03-06 20:23:04 +0800

    Added Bermejo.java with age calculation and updated week5.java

---
+++ Bermejo.java
@@ -0,0 +1,8 @@
+public class Bermejo {
+    public static void display() {
+        int age = 22; // Replace with user input if necessary
+        System.out.println("Hello! This is Bermejo contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
