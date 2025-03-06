2656fba98d47012798192b5191bddd0fbc6ed882
ivanrolf <irgeringer@paterostechnologicalcollege.edu.ph>
2025-03-06 19:50:29 +0800

    Added Geringer.java with age calculation and updated week5.java

--- week5.java
+++ week5.java
@@ -3,10 +3,14 @@ public class week5 {
         System.out.println("Collaborative Java Project\n");
 
         // Call each student's method
+
+        Sonajo.display();//lastname.display(); // Add your method call here
+
         //lastname.display(); // Add your method call here
 
         Piano.display();
         Geringer.display();
+
 
     }
 }
