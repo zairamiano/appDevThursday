cfd45ece24829dc88b6824baefdc99bdc3bd9708
Tejado <ltejado@paterostechnologicalcollege.edu.ph>
2025-03-06 19:44:56 +0800

    Added Tejado.java with age calculation and updated week5.java

--- week5.java
+++ week5.java
@@ -3,34 +3,14 @@ public class week5 {
         System.out.println("Collaborative Java Project\n");
 
         // Call each student's method
-
         ClarkKurt.display(); 
-
         Hernandez.display(); 
-
         Smith.display(); 
-
-
         Sonajo.display();
-
-       
-
         Pascua.display();
-
-
         estonilo.display(); 
-
-
         Piano.display();
         Hernandez.display();
-
-
-       
-
-
-
-
-
+        Tejado.display();
     }
-
 }
