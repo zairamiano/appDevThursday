f6e64187bab4c65a590269a525f034b8642a5f6e
Vincent1830 <vscequena@paterostechnologicalcollege.edu.ph>
2025-03-07 16:08:14 +0800

    Create Cequena.java

---
+++ Cequena.java
@@ -0,0 +1,8 @@
+public class Cequena {
+    public static void display() {
+        int age = 29;
+        System.out.println("Hello! This is Cequena contributing to the project.");
+        System.out.println("My current age is: " + age);
+        System.out.println("Next year, I will be: " + (age + 1));
+    }
+}
