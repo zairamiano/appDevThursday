93728747aa9a1df0a9213ad9def91b0255360d40
Jardeleza0921 <jjardeleza@paterostechnologicalcollege.edu.ph>
2025-03-16 23:22:12 +0800

    Group-6-Added-Square-Method-(Members-JARDELEZA,-SAN-MIGUEL,-GABRILLO,-SANTANDER,-HALLIG,-ORDINARIO,-MUYRONG,-ABOHAN,-ANTONIO)

--- GroupWork.java
+++ GroupWork.java
@@ -8,6 +8,11 @@ public class GroupWork {
         System.out.print("Enter second number: ");
         int num2 = scanner.nextInt();
 
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        double square = number * number;
+        System.out.println("The square of " + number + " is: " + square); //Group 6's method
+
         System.out.println("Cube: " + Calculator.cube(num1));  // Group 7's method
         System.out.println("Product: " + Calculator.multiply(num1, num2)); // Group 3's method
         System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
@@ -16,5 +21,6 @@ public class GroupWork {
         System.out.println("Square Root: " + Calculator.sqrt(num1));  // Group 11's method
         System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4's
         System.out.println("Modulus: " + Calculator.modulus(num1, num2));  // Group 5 method
+        System.out.println("Square: " + Calculator.square(num1));  // Group 6's method
      }
 }
