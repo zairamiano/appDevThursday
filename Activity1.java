fbcde8542b0ac072e06c0af0992a7e25cc6acbc5
zairamiano <zairamiano7@gmail.com>
2025-04-07 17:25:54 +0800

    Create Activity1.java

---
+++ Activity1.java
@@ -0,0 +1,8 @@
+public class Activity1 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+   
+      
+        // System.out.println("Zairamie Miano - BSIT 2L");
+    }
+}
